63dbaa3b716e7c1770cb7bdbf6e23199cdc2a555
alonshlomi1 <alonasshlomi@gmail.com>
2024-01-29 11:36:25 +0200

    add backround

--- app/src/main/java/com/example/spaceexplorerapp/MainActivity.java
+++ app/src/main/java/com/example/spaceexplorerapp/MainActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ShapeableImageView main_IMG_background;
+    private ShapeableImageView[] main_IMG_hearts;
+    private MaterialTextView main_LBL_score;
+    private ExtendedFloatingActionButton main_FAB_left;
+    private ExtendedFloatingActionButton main_FAB_right;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        Glide.with(this)
+                .load(R.drawable.space_backround)
+                .centerCrop()
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(main_IMG_background);
+    }
+
+    private void findViews() {
+        main_IMG_hearts = new ShapeableImageView[]{
+                findViewById(R.id.main_IMG_heart1),
+                findViewById(R.id.main_IMG_heart2),
+                findViewById(R.id.main_IMG_heart3),
+        };
+        main_LBL_score= findViewById(R.id.main_LBL_score);
+        main_FAB_left = findViewById(R.id.main_FAB_left);
+        main_FAB_right = findViewById(R.id.main_FAB_right);
+        main_IMG_background = findViewById(R.id.main_IMG_background);
     }
 }
